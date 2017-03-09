6ccd1d20ff748e382ee469cd48f901b8e8a9eac3
gabrieledcjr <gabrieledcjr@gmail.com>
2017-03-08 18:05:19 -0800

    use gym's built-in keyboard controller for human demo

--- envs/gym_fun.py
+++ envs/gym_fun.py
@@ -53,22 +53,36 @@ class GameState:
             self._env = gym.make('BreakoutDeterministic-v3')
             self.n_actions = 4
             print (colored("BreakoutDeterministic-v3", "green"))
+        elif self.game == 'freeway':
+            self._env = gym.make('FreewayDeterministic-v3')
+            self.n_actions = 3
+            print (colored("FreewayDeterministic-v3", "green"))
+
         self._env.frameskip = frame_skip
         self.lives = self._env.ale.lives()
         print (colored("lives: {}".format(self.lives), "green"))
         print (colored("frameskip: {}".format(self._env.frameskip), "green"))
-        #print (colored("repeat_action_probability: {}".format(self._env.ale.getFloat('repeat_action_probability')), "green"))
-        sleep(2)
+        print (colored("repeat_action_probability: {}".format(self._env.ale.getFloat(b'repeat_action_probability')), "green"))
 
         self._human_demo = human_demo
+        print (colored("human_demo: {}".format(self._human_demo), "green" if self._human_demo else "red"))
         if self._human_demo:
-            self._screen = pygame.display.set_mode((240,320),0,32)
+            self.human_agent_action = 0
+            self.human_agent_action_code = 0
+            self.human_wants_restart = False
+            self.human_sets_pause = False
+            self._env.render(mode='human')
+            self._env.unwrapped.viewer.window.on_key_press = self.key_press
+            self._env.unwrapped.viewer.window.on_key_release = self.key_release
+            #self._screen = pygame.display.set_mode((240,320),0,32)
+
+        sleep(2)
         self.reinit()
 
-    def reinit(self, gui=False, random_restart=False, is_testing=False):
-        self.lost_life = False
-        self.is_testing = is_testing
-        if gui:
+    def reinit(self, render=False, random_restart=False, terminate_loss_of_life=True):
+        # self.loss_life = False
+        self.terminate_loss_of_life = terminate_loss_of_life
+        if render or self._human_demo:
             self._env.render(mode='human')
         self._env.reset()
         self.lives = self._env.ale.lives()
@@ -99,7 +113,32 @@ class GameState:
 
         return action_index
 
-    def frame_step(self, act, gui=False, random_restart=False):
+    def key_press(self, key, mod):
+        if key==0xff0d: self.human_wants_restart = True
+        if key==32: self.human_sets_pause = not self.human_sets_pause
+        a = int( key - ord('0'))
+        if (a > 0 and a <= 9) or (a >= 65409 and a <= 65417):
+            self.human_agent_action_code = a
+            if a == 8 or a == 65416: # UP
+                self.human_agent_action = 1
+            elif a == 2 or a == 65410: # DOWN
+                self.human_agent_action = 2
+            elif a == 4 or a == 65412: # LEFT
+                self.human_agent_action = 1
+            elif a == 6 or a == 65414: # RIGHT
+                self.human_agent_action = 2
+            elif a == 5 or a == 65413: # FIRE
+                self.human_agent_action = 3
+            else: # NOOP
+                self.human_agent_action = 0
+
+    def key_release(self, key, mod):
+        a = int( key - ord('0') )
+        if (a > 0 and a <= 9) or (a >= 65409 and a <= 65417):
+            if self.human_agent_action_code == a:
+                self.human_agent_action = 0
+
+    def frame_step(self, act, render=False, random_restart=False):
         if self.game == 'pong':
             if act == 1:#Key up
                 action = UP
@@ -116,43 +155,57 @@ class GameState:
                 action = FIRE
             else: # don't move
                 action = 0
-            if self._human_demo and self.lost_life:
-                action = FIRE # fire automatically just during HUMAN DEMO
-                self.lost_life = False
+            # if self._human_demo and self.loss_life:
+            #     action = FIRE # fire automatically just during HUMAN DEMO
+            #     self.loss_life = False
+        elif self.game == 'freeway':
+            if act == 1:
+                action = 1 # UP
+            elif act == 2:
+                action = 2 # DOWN
+            else:
+                action = 0
 
         observation, reward, terminal, info = self._env.step(action)
         self.screen_buffer = observation
 
         if (self.lives - info['ale.lives']) != 0:
-            self.lost_life = True
+            # self.loss_life = True
             self.lives -= 1
             # Consider terminal state after LOSS OF LIFE not after episode ends
-            if not self.is_testing:
+            if self.terminate_loss_of_life:
                 terminal = True
 
-        if self._human_demo:
-            surface = pygame.surfarray.make_surface(observation)
-            surface = pygame.transform.flip(surface, False, True)
-            surface = pygame.transform.rotate(surface, -90)
-            surface = pygame.transform.scale(surface, (240,320))
-            bv = self._screen.blit(surface, (0,0))
-            pygame.display.flip()
-        if gui:
+        # if self._human_demo:
+        #     surface = pygame.surfarray.make_surface(observation)
+        #     surface = pygame.transform.flip(surface, False, True)
+        #     surface = pygame.transform.rotate(surface, -90)
+        #     surface = pygame.transform.scale(surface, (240,320))
+        #     bv = self._screen.blit(surface, (0,0))
+        #     pygame.display.flip()
+        if render or self._human_demo:
             self._env.render(mode='human')
-
-        # frame = _process_frame42(observation)
-        # frame = observation[34:34+160, :160]
-        # cv2.imshow("observation", frame)
-        # cv2.waitKey(2)
 
         if terminal:
             self.reinit(random_restart=random_restart)
 
         return observation, reward, (1 if terminal else 0)
 
-# test_game = GameState()
+# test_game = GameState(human_demo=True, game='freeway')
+# test_game.reinit(render=True)
 # terminal = False
-# while not terminal:
-#     test_game.frame_step(random.choice([0,1,2]))
-#     import time
-#     time.sleep(2)
+# skip = 0
+# for t in range(2000):
+#     a = test_game.human_agent_action
+#     # if not skip:
+#     #     a = test_game.human_agent_action
+#     #     skip = 0
+#     # else:
+#     #     skip -= 1
+#     _, r, terminal = test_game.frame_step(a, render=True)
+#     sleep(0.06)
+#     if terminal: break
+#     if test_game.human_wants_restart: break
+#     while test_game.human_sets_pause:
+#         test_game._env.render(mode='human')
+#         sleep(0.1)
