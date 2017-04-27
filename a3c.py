add testing model, fix bug on not normalizing input in classification

a3c.py
@@ -14,6 +14,11 @@ from game_ac_network import GameACFFNetwork, GameACLSTMNetwork
 from a3c_training_thread import A3CTrainingThread
 from rmsprop_applier import RMSPropApplier
 
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
 def run_a3c(args):
     """
     python3 a3c.py --gym-env=PongDeterministic-v3 --parallel-size=16 --initial-learn-rate=7e-4 --use-lstm --use-mnih-2015
@@ -49,6 +54,9 @@ def run_a3c(args):
         end_str += '_use_lstm'
     folder += end_str
 
+    if args.append_experiment_num is not None:
+        folder += '_' + args.append_experiment_num
+
     device = "/cpu:0"
     gpu_options = None
     if args.use_gpu:
@@ -66,6 +74,7 @@ def run_a3c(args):
     time.sleep(2)
 
     global_t = 0
+    testing_rewards = {}
 
     stop_requested = False
 
@@ -167,7 +176,7 @@ def run_a3c(args):
         sess.run(init)
 
     # summary for tensorboard
-    score_input = tf.placeholder(tf.int32)
+    score_input = tf.placeholder(tf.float32)
     tf.summary.scalar("score", score_input)
 
     summary_op = tf.summary.merge_all()
@@ -187,19 +196,23 @@ def run_a3c(args):
         wall_t_fname = folder + '/' + 'wall_t.' + str(global_t)
         with open(wall_t_fname, 'r') as f:
             wall_t = float(f.read())
+        testing_rewards = pickle.load(open(folder + '/' + args.gym_env.replace('-', '_') + '-rewards.pkl', 'rb'))
     else:
         print(colored("Could not find old checkpoint", "yellow"))
         # set wall time
         wall_t = 0.0
 
-
+    lock = threading.Lock()
+    test_lock = False
+    last_temp_global_t = global_t
     def train_function(parallel_index):
-        nonlocal global_t
-
+        nonlocal global_t, testing_rewards, test_lock, lock, last_temp_global_t
         training_thread = training_threads[parallel_index]
         # set start_time
         start_time = time.time() - wall_t
         training_thread.set_start_time(start_time)
+
+        diff_global_t = 0
 
         while True:
             if stop_requested:
@@ -207,10 +220,31 @@ def run_a3c(args):
             if global_t > args.max_time_step:
                 break
 
+            for _ in range(diff_global_t):
+                global_t += 1
+                if global_t % args.eval_freq == 0:
+                    temp_global_t = global_t
+                    lock.acquire()
+                    try:
+                        # catch multiple threads getting in at the same time
+                        if last_temp_global_t == temp_global_t:
+                            print(colored("Threading race problem averted!", "blue"))
+                            continue
+                        test_lock = True
+                        test_reward = training_thread.testing(
+                            sess, args.eval_max_steps, temp_global_t,
+                            summary_writer)
+                        testing_rewards[temp_global_t] = test_reward
+                        test_lock = False
+                        last_temp_global_t = temp_global_t
+                    finally:
+                        lock.release()
+                while test_lock:
+                    time.sleep(0.01)
+
             diff_global_t = training_thread.process(
                 sess, global_t, summary_writer,
                 summary_op, score_input)
-            global_t += diff_global_t
 
 
     def signal_handler(signal, frame):
@@ -226,6 +260,12 @@ def run_a3c(args):
 
     # set start time
     start_time = time.time() - wall_t
+
+    if global_t == 0:
+        test_reward = training_threads[0].testing(
+            sess, args.eval_max_steps, global_t,
+            summary_writer)
+        testing_rewards[global_t] = test_reward
 
     for t in train_threads:
         t.start()
@@ -248,4 +288,6 @@ def run_a3c(args):
         f.write(str(wall_t))
 
     saver.save(sess, folder + '/' + '{}_checkpoint'.format(args.gym_env.replace('-', '_')), global_step = global_t)
+
+    pickle.dump(testing_rewards, open(folder + '/' + args.gym_env.replace('-', '_') + '-rewards.pkl', 'wb'), pickle.HIGHEST_PROTOCOL)
     print (colored('Data saved!', 'green'))

a3c_training_thread.py
@@ -8,6 +8,7 @@ import sys
 from termcolor import colored
 from game_state import GameState
 from game_ac_network import GameACFFNetwork, GameACLSTMNetwork
+from util import get_action_index
 
 
 class A3CTrainingThread(object):
@@ -86,13 +87,74 @@ class A3CTrainingThread(object):
 
   def _record_score(self, sess, summary_writer, summary_op, score_input, score, global_t):
     summary_str = sess.run(summary_op, feed_dict={
-      score_input: score
+      score_input: float(score)
     })
     summary_writer.add_summary(summary_str, global_t)
     summary_writer.flush()
 
   def set_start_time(self, start_time):
     self.start_time = start_time
+
+  def testing(self, sess, max_steps, global_t, summary_writer):
+    # copy weights from shared to local
+    sess.run( self.sync )
+
+    self.game_state.reset()
+    if self.use_lstm:
+      self.local_network.reset_state()
+
+    total_rewards = 0.
+    total_steps = 0.
+    episode_count = 0
+    while True:
+      if self.use_lstm:
+        start_lstm_state = self.local_network.lstm_state_out
+
+      episode_reward = 0
+      while total_steps < max_steps:
+        pi_ = self.local_network.run_policy(sess, self.game_state.s_t)
+        #action = self.choose_action(pi_)
+        action = get_action_index(pi_, is_random=(np.random.random() <= 0.05), n_actions=self.game_state.env.n_actions)
+
+        # process game
+        self.game_state.process(action)
+
+        # receive game result
+        reward = self.game_state.reward
+        terminal = self.game_state.terminal
+
+        episode_reward += reward
+        total_steps += 1
+
+        # s_t1 -> s_t
+        self.game_state.update()
+
+        if terminal:
+          total_rewards += episode_reward
+          episode_count += 1
+          print(colored("test: global_t={} t_idx={} score={} total_steps={}".format(global_t, self.thread_index, episode_reward, total_steps), "yellow"))
+
+          self.game_state.reset()
+          if self.use_lstm:
+            self.local_network.reset_state()
+          break
+
+      if total_steps >= max_steps:
+          break
+
+    testing_reward = total_rewards / episode_count
+    print(colored("test: global_t={} t_idx={} final score={}".format(global_t, self.thread_index, testing_reward), "green"))
+
+    summary = tf.Summary()
+    summary.value.add(tag='Testing/score', simple_value=float(testing_reward))
+    summary_writer.add_summary(summary, global_t)
+    summary_writer.flush()
+
+    self.episode_reward = 0
+    self.game_state.reset()
+    if self.use_lstm:
+      self.local_network.reset_state()
+    return testing_reward
 
   def process(self, sess, global_t, summary_writer, summary_op, score_input):
     states = []

classify_demo.py
@@ -38,7 +38,19 @@ class ClassifyDemo(object):
 
         self.net.prepare_loss()
         self.net.prepare_evaluate()
-        self.apply_gradients = grad_applier.minimize(self.net.total_loss)
+        grads_vars = grad_applier.compute_gradients(self.net.total_loss)
+        grads = []
+        params = []
+        for p in grads_vars:
+            if p[0] == None:
+                continue
+            grads.append(p[0])
+            params.append(p[1])
+
+        #grads = tf.clip_by_global_norm(grads, 1)[0]
+        grads_vars_updates = zip(grads, params)
+        self.apply_gradients = grad_applier.apply_gradients(grads_vars_updates)
+        #self.apply_gradients = grad_applier.minimize(self.net.total_loss)
 
     def _load_memory(self):
         assert os.path.isfile(self.demo_memory_folder + '/demo.db')
@@ -82,9 +94,18 @@ class ClassifyDemo(object):
         D_size = len(self.D)
 
         for i in range(self.train_max_steps):
-            idx = np.random.randint(0, D_size)
+            # batch_si = []
+            # batch_a = []
+            # for _ in range(self.batch_size):
+            #     idx = np.random.randint(0, D_size)
+            #     samp_idx = np.random.randint(0, len(self.D[idx]))
+            #     s, ai, _, _ = self.D[idx][samp_idx]
+            #     batch_si.append(s * (1.0/255.0))
+            #     a = np.zeros([self.net._action_size])
+            #     a[ai] = 1
+            #     batch_a.append(a)
 
-            batch_si, batch_a, _, _ = self.D[idx].random_batch(self.batch_size)
+            batch_si, batch_a, _, _ = self.D[idx].random_batch(self.batch_size, normalize=True)
             train_loss, acc, max_value, _ = sess.run(
                 [self.net.total_loss, self.net.accuracy, self.net.max_value, self.apply_gradients],
                 feed_dict = {
@@ -136,7 +157,7 @@ class ClassifyDemo(object):
             count_batch = 0
             while True:
                 s, ai, _, terminal = self.D[idx][last_index]
-                batch_si.append(s)
+                batch_si.append(s * (1.0/255.0))
                 a = np.zeros([self.net._action_size])
                 a[ai] = 1
                 batch_a.append(a)
@@ -175,7 +196,7 @@ class ClassifyDemo(object):
 
 def classify_demo(args):
     '''
-    python3 run_experiment.py --gym-env=PongDeterministic-v3 --classify-demo --use-lstm --use-mnih-2015
+    python3 run_experiment.py --gym-env=PongDeterministic-v3 --classify-demo --use-mnih-2015 --max-time-step=150000 --local-t-max=32
     '''
     if args.use_gpu:
         assert args.cuda_devices != ''

data_set.py
@@ -110,7 +110,7 @@ class DataSet(object):
     def get_item(self, key):
         return self.__getitem__(key)
 
-    def random_batch(self, batch_size):
+    def random_batch_sequential(self, batch_size):
         """Return corresponding states, actions, rewards, terminal status, and
         next_states for batch_size randomly chosen state transitions.
 
@@ -140,6 +140,35 @@ class DataSet(object):
 
         return states, actions, rewards, terminals
 
+    def random_batch(self, batch_size, normalize=False):
+        """Return corresponding states, actions, rewards, terminal status, and
+        next_states for batch_size randomly chosen state transitions.
+
+        """
+        # Allocate the response.
+        states = np.zeros((batch_size, 84, 84, self.phi_length), dtype=np.uint8)
+        actions = np.zeros((batch_size, self.num_actions), dtype=np.float32)
+        rewards = np.zeros(batch_size, dtype=np.float32)
+        terminals = np.zeros(batch_size, dtype=np.int)
+
+        # Randomly choose a time step from the replay memory
+        # within requested batch_size
+        end_range = self.size - self.phi_length
+        assert end_range > 0 # crash if not enough memory
+
+        for count in range(batch_size):
+            index = self.rng.randint(0, end_range)
+            s, a, r, t = self[index]
+            # Add the state transition to the response.
+            states[count] = np.copy(s)
+            if normalize:
+                states[count] *= (1.0/255.0)
+            actions[count][a] = 1. # convert to one-hot vector
+            rewards[count] = r
+            terminals[count] = t
+
+        return states, actions, rewards, terminals
+
 def test_1(env_id):
     from util import get_compressed_images
     try:
@@ -163,8 +192,7 @@ def test_1(env_id):
     print (D)
 
     for i in range(1000):
-        D.random_batch(20)
-        print (D.random_index)
+        states, actions, rewards, terminals = D.random_batch(20)
 
     import cv2
     count = 0