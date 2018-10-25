0435351a8e6d49c1d68bf7d12808231ec420331a
Gabriel de la Cruz <gabrieledcjr@gmail.com>
2018-10-25 12:07:59 -0700

    clip by norm to clip by global norm

--- a3c/rmsprop_applier.py
+++ a3c/rmsprop_applier.py
@@ -108,10 +108,11 @@ class RMSPropApplier(object):
             with tf.control_dependencies(None):
                 self._create_slots(var_list)
 
+            grads, grad_norm = tf.clip_by_global_norm(accum_grad_list, self._clip_norm)
             with tf.name_scope(name, self._name, []) as name:
                 self._prepare()
-                for var, accum_grad in zip(var_list, accum_grad_list):
+                for var, accum_grad in zip(var_list, grads):
                     with tf.name_scope("update_" + var.op.name), tf.device(var.device):
-                        clipped_accum_grad = tf.clip_by_norm(accum_grad, self._clip_norm)
-                        update_ops.append(self._apply_dense(clipped_accum_grad, var))
+                        #clipped_accum_grad = tf.clip_by_norm(accum_grad, self._clip_norm)
+                        update_ops.append(self._apply_dense(accum_grad, var))
                 return tf.group(*update_ops, name=name)
